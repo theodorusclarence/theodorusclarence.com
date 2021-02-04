54bc0d620332de1b2495d016d9596524808335c6
Theodorus Clarence <55318172+theodorusclarence@users.noreply.github.com>
2021-02-04 20:20:17 +0700

    Develop (#14)
    * added ui fundamental post
    
    * added new post to featured
    
    * added transition on hover for cards and button
    
    * added accent on button hover

--- components/LibraryCard.jsx
+++ components/LibraryCard.jsx
@@ -5,9 +5,11 @@ export default function LibraryCard({ post, slug }) {
     const techArray = post.techs.split(',');
     return (
         <Link href={`/library/${slug}`}>
-            <a className='h-full p-4 transition-shadow duration-100 rounded-md group border-thin active:shadow-none hover:shadow-md ring-vis-0'>
+            <a className='h-full p-4 transition-shadow duration-100 rounded-md card group border-thin active:shadow-none hover:shadow-md ring-vis-0'>
                 <div>
-                    <h4>{post.title}</h4>
+                    <h4>
+                        <span>{post.title}</span>
+                    </h4>
                     <PickTech techs={techArray} />
                     <p className='component'>{post.description}</p>
                 </div>
