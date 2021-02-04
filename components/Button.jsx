8dbd0e1a4f4004662f2b7b86e3753f4f69f30daa
Theodorus Clarence <theodorusclarence@gmail.com>
2021-02-04 20:17:02 +0700

    added accent on button hover

--- components/Button.jsx
+++ components/Button.jsx
@@ -6,7 +6,7 @@ export default function Button({ href, children, className }) {
             <a
                 className={`btn ${className} ring-vis-0 inline-block px-4 py-2 font-medium transition-shadow duration-100 rounded-md active:shadow-none hover:shadow-md border-thin`}
             >
-                {children}
+                <span>{children}</span>
             </a>
         </Link>
     );
