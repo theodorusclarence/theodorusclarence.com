ccadb7f6f055c1ef4a3665ecfa063468b4ba8022
Theodorus Clarence <theodorusclarence@gmail.com>
2021-05-10 12:19:34 +0700

    feat: add CloudinaryImg component

---
+++ components/CloudinaryImg.jsx
@@ -0,0 +1,50 @@
+import Image from 'next/image';
+import { buildUrl } from 'cloudinary-build-url';
+
+export default function CloudinaryImg({ publicId, height, width }) {
+    const urlBlurred = buildUrl(publicId, {
+        cloud: {
+            cloudName: 'theodorusclarence',
+        },
+        transformations: {
+            effect: {
+                name: 'blur:1000',
+                quality: 1,
+            },
+        },
+    });
+    const url = buildUrl(publicId, {
+        cloud: {
+            cloudName: 'theodorusclarence',
+        },
+    });
+
+    return (
+        <div
+            style={{
+                position: 'relative',
+                height: 0,
+                paddingTop: `${(height / width) * 100}%`,
+                backgroundImage: `url(${urlBlurred})`,
+                backgroundPosition: 'center center',
+                backgroundSize: `100%`,
+            }}
+        >
+            <div
+                style={{
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                }}
+            >
+                <Image
+                    width={width}
+                    height={height}
+                    src={url}
+                    alt='Cloudinary'
+                    unoptimized={true}
+                />
+            </div>
+        </div>
+    );
+}
