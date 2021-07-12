cb02f0d106749a391fe43566de4ab1668f67ce60
Theodorus Clarence <theodorusclarence@gmail.com>
2021-07-12 12:13:12 +0700

    fix: debounce mutation

--- utils/helper.js
+++ utils/helper.js
@@ -42,3 +42,20 @@ export const ogGenerate = (title, type = 'Blog') => {
 export function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
 }
+
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// `wait` milliseconds.
+export const debounce = (func, wait) => {
+  let timeout;
+
+  return function executedFunction(...args) {
+    const later = () => {
+      clearTimeout(timeout);
+      func(...args);
+    };
+
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+  };
+};
