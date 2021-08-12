1c63a74014f14a0a077c13158028295c17aebcca
Theodorus Clarence <theodorusclarence@gmail.com>
2021-08-12 14:24:00 +0700

    feat: add lightbulb to 404 page

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,4 +1,5 @@
 import { ReactFlashlight } from 'react-flashlight';
+import { GiLightBulb } from 'react-icons/gi';
 
 import UnstyledLink from '@/components/UnstyledLink';
 import Seo from '@/components/Seo';
@@ -14,10 +15,14 @@ export default function Custom404() {
         initialPosition={{ x: 187, y: 400 }}
       >
         <div className='flex flex-col items-center justify-center min-h-screen'>
-          <h1>Uh Oh! You lost?</h1>
+          <GiLightBulb
+            size={60}
+            className='text-yellow-300 animate-flicker drop-shadow-glow'
+          />
+          <h1 className='mt-8'>Page Not Found</h1>
           <UnstyledLink
             href='/'
-            className='p-8 mt-8 text-3xl font-bold hover:text-accent-300'
+            className='p-8 mt-4 text-3xl font-bold hover:text-accent-300'
           >
             Back To Home
           </UnstyledLink>
