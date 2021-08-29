67cdfca04a1ad6a036d54d578f3f7e73bbfd72b9
Theodorus Clarence <theodorusclarence@gmail.com>
2021-08-29 20:59:46 +0700

    feat: add footer links and twitter

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
 import Tippy from '@tippyjs/react';
 import { FiMail } from 'react-icons/fi';
-import { SiGithub, SiLinkedin } from 'react-icons/si';
+import { SiGithub, SiLinkedin, SiTwitter } from 'react-icons/si';
 import CopyToClipboard from 'react-copy-to-clipboard';
 
 import UnstyledLink from './UnstyledLink';
+import CustomLink from './CustomLink';
 import SpotifyPlaying from './SpotifyPlaying';
 
 export default function Footer() {
@@ -13,8 +14,9 @@ export default function Footer() {
   return (
     <footer className='pb-2 mt-auto'>
       <main className='flex flex-col items-center pt-6 border-t-thin dark:border-gray-600 layout'>
-        <p className='mb-1 font-medium'>Reach me out</p>
-        <div className='flex mb-4 space-x-4'>
+        <FooterLinks />
+        <p className='mt-8 font-medium'>Reach me out</p>
+        <div className='flex mt-2 space-x-4'>
           <Tippy
             animation='scale-subtle'
             interactive={true}
@@ -60,12 +62,53 @@ export default function Footer() {
           >
             <SiLinkedin className='w-6 h-6 my-auto align-middle text-dark dark:text-light hover:text-accent-200 dark:hover:text-accent-200' />
           </UnstyledLink>
+          <UnstyledLink
+            className='inline-flex items-center justify-center rounded-sm ring-vis'
+            href='https://clarence.link/twt'
+          >
+            <SiTwitter className='w-6 h-6 my-auto align-middle text-dark dark:text-light hover:text-accent-200 dark:hover:text-accent-200' />
+          </UnstyledLink>
         </div>
         <SpotifyPlaying />
-        <p className='mt-4 text-xs font-dark '>
+        <p className='mt-8 text-xs font-dark '>
           © Theodorus Clarence {new Date().getFullYear()}
         </p>
       </main>
     </footer>
   );
 }
+
+function FooterLinks() {
+  return (
+    <div className='grid justify-center w-full grid-cols-2 gap-2 sm:gap-8 justify-items-start sm:flex'>
+      {footerLinks.map(({ href, text }) => (
+        <UnstyledLink
+          key={href}
+          className='text-sm font-medium rounded-sm view dark:text-gray-200 animated-underline ring-vis'
+          href={href}
+        >
+          {text}
+        </UnstyledLink>
+      ))}
+    </div>
+  );
+}
+
+const footerLinks = [
+  {
+    href: '/projects/side-projects',
+    text: 'Side Projects',
+  },
+  {
+    href: 'https://clarence.link/booknotes',
+    text: 'Booknotes',
+  },
+  {
+    href: 'https://pw.theodorusclarence.com',
+    text: 'Polywork',
+  },
+  {
+    href: 'https://clarence.link/starters',
+    text: 'Starter Templates',
+  },
+];
