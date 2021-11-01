4b0387ea3841d8ee633ab9fb1434fe453ddb42ff
Theodorus Clarence <theodorusclarence@gmail.com>
2021-11-01 22:45:05 +0700

    feat: add copy button to code

--- components/CustomCode.jsx
+++ components/CustomCode.jsx
@@ -1,3 +1,7 @@
+import * as React from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { HiClipboard, HiCheckCircle } from 'react-icons/hi';
+
 export function Pre(props) {
   return (
     <pre {...props}>
@@ -13,16 +17,37 @@ export function Pre(props) {
 }
 
 export default function CustomCode(props) {
+  const textRef = React.useRef(null);
+  const [isCopied, setIsCopied] = React.useState(false);
+
   const language = props.className?.includes('language')
     ? props.className.replace('language-', '')
     : null;
+
   return (
     <code {...props}>
-      {props.children}
+      <span ref={textRef}>{props.children}</span>
       {language && (
         <div className='absolute top-0 px-3 py-1 border-t-0 rounded-b-md border-thin left-6'>
           <span className='font-medium select-none accent'>{language}</span>
         </div>
+      )}
+      {language && (
+        <CopyToClipboard
+          text={textRef?.current?.textContent}
+          onCopy={() => {
+            setIsCopied(true);
+            setTimeout(() => setIsCopied(false), 1500);
+          }}
+        >
+          <button className='absolute hidden p-2 text-lg transition-colors rounded md:block border-thin hover:bg-gray-700 top-2 right-2'>
+            {isCopied ? (
+              <HiCheckCircle className='text-green-400' />
+            ) : (
+              <HiClipboard />
+            )}
+          </button>
+        </CopyToClipboard>
       )}
     </code>
   );
