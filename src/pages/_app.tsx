9938c03908f4665764dc7f15c4d1816b1f074173
Theodorus Clarence <theodorusclarence@gmail.com>
2021-11-01 23:49:43 +0700

    chore(package): add react-tippy

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 
+import 'react-tippy/dist/tippy.css';
 import '@/styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
