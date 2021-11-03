01f55e42931bce720a5b5b3a4228cdc3c981b497
Theodorus Clarence <theodorusclarence@gmail.com>
2021-11-03 17:38:31 +0700

    chore: use preact for production

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+/**
+ * @type {import('next').NextConfig}
+ */
 module.exports = {
   eslint: {
     dirs: ['src'],
@@ -9,5 +12,17 @@ module.exports = {
       // Spotify Album
       'i.scdn.co',
     ],
+  },
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
+    }
+
+    return config;
   },
 };
