a298283fed1e51b9c6b14afa5488849678ba4f0b
Theodorus Clarence <theodorusclarence@gmail.com>
2021-11-05 16:09:00 +0700

    feat: add trf page and side-projects

---
+++ src/pages/trf.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import CopyToClipboard from 'react-copy-to-clipboard';
+
+import Accent from '@/components/Accent';
+import Button from '@/components/buttons/Button';
+import CloudinaryImg from '@/components/CloudinaryImg';
+import Layout from '@/components/layout/Layout';
+import Seo from '@/components/Seo';
+
+const copyText = {
+  idle: 'Copy Nomor Rekening',
+  success: 'Copied to clipboard 🥳',
+};
+
+export default function TrfPage() {
+  const [copyStatus, setCopyStatus] = React.useState<string>(copyText.idle);
+
+  return (
+    <Layout>
+      <Seo
+        templateTitle='Rekening Clarence - theodorusclarence.com'
+        description='Buka untuk melihat QR Code dan Nomor Rekening Clarence'
+        robots='noindex,nofollow'
+      />
+
+      <main>
+        <section className=''>
+          <div className='flex flex-col items-center py-12 text-center layout'>
+            <h1>Rekening BCA</h1>
+            <p className='mt-4 text-sm font-bold text-gray-500 uppercase dark:text-gray-400'>
+              Atas Nama
+            </p>
+            <h3 className='mt-1'>
+              <Accent>CLARENCE</Accent>
+            </h3>
+            <CloudinaryImg
+              className='w-full max-w-sm mt-4 overflow-hidden rounded shadow-sm dark:shadow-none'
+              publicId='theodorusclarence/qr-bca_rcbkew.jpg'
+              width={992}
+              height={886}
+              alt='QR BCA'
+            />
+            <div className='flex flex-col items-center mt-8 space-y-4'>
+              <code className='px-4 py-2 font-bold'>
+                <Accent>7630055037</Accent>
+              </code>
+              <CopyToClipboard
+                text='7630055037'
+                onCopy={() => {
+                  setCopyStatus(copyText.success);
+                  setTimeout(() => setCopyStatus(copyText.idle), 1500);
+                }}
+              >
+                <Button>{copyStatus}</Button>
+              </CopyToClipboard>
+            </div>
+          </div>
+        </section>
+      </main>
+    </Layout>
+  );
+}
