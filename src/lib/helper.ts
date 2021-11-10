180b81830bb628b39276ef41e46089f873de68fd
Theodorus Clarence <theodorusclarence@gmail.com>
2021-11-10 16:28:13 +0700

    feat: use gradient og

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -21,7 +21,7 @@ export function openGraph({
     : undefined;
   const ogDesc = encodeURIComponent(description.trim());
 
-  return `https://og.thcl.dev/api/general?siteName=${ogSiteName}&description=${ogDesc}&logo=${ogLogo}${
+  return `https://og.thcl.dev/api/gradient?siteName=${ogSiteName}&description=${ogDesc}&logo=${ogLogo}${
     ogTemplateTitle ? `&templateTitle=${ogTemplateTitle}` : ''
   }`;
 }
