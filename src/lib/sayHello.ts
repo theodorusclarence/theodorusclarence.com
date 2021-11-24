bcf39e481eebd3dfbd0f7c81d2da1443e84fceaa
Theodorus Clarence <theodorusclarence@gmail.com>
2021-11-24 15:53:57 +0700

    feat: add initial sayHello

---
+++ src/lib/sayHello.ts
@@ -0,0 +1,10 @@
+/* eslint-disable no-console */
+export default function sayHello() {
+  console.log(
+    '%cHello developers!',
+    'font-size: 4rem; color: cyan; font-family: inter; font-weight: 700; background: linear-gradient(to top right, #00e887, #00e0f3); background-clip:text; color: transparent;'
+  );
+  console.log(
+    "Looks like you are interested in the code 👀\nHere is the repo to fulfill your curiosity https://github.com/theodorusclarence/theodorusclarence.com\n\nDon't forget to give it a star! 🌟"
+  );
+}
