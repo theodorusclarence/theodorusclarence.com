1390fa33e1a1b3d66204aa2ce5087fd8a674c5bc
Theodorus Clarence <theodorusclarence@gmail.com>
2021-11-24 15:53:57 +0700

    feat: add initial sayHello

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,10 +1,20 @@
 import * as React from 'react';
 
+import sayHello from '@/lib/sayHello';
+
 import Footer from '@/components/layout/Footer';
 import Header from '@/components/layout/Header';
 
 import { PreloadProvider } from '@/context/PreloadContext';
+
+let saidHello = false;
+
 export default function Layout({ children }: { children: React.ReactNode }) {
+  if (!saidHello) {
+    sayHello();
+    saidHello = true;
+  }
+
   return (
     <>
       <Header />
