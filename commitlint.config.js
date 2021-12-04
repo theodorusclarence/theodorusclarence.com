38c5c66e3535313ac2bd2cac82d71e19dc2c267d
Theodorus Clarence <theodorusclarence@gmail.com>
2021-12-04 14:12:23 +0700

    ci: add commitlint scope-enum

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    //   TODO Add Scope Enum Here
-    // 'scope-enum': [2, 'always', ['yourscope', 'yourscope']],
+    'scope-enum': [2, 'always', ['blog', 'proj', 'lib', 'package']],
     'type-enum': [
       2,
       'always',
