c73c4774632eab46acdba47290c0aa2a8c880025
Theodorus Clarence <theodorusclarence@gmail.com>
2021-12-29 10:10:11 +0700

    feat: add copywrite and status for dev

--- src/pages/dev/[pw].tsx
+++ src/pages/dev/[pw].tsx
@@ -1,12 +1,21 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 import * as React from 'react';
 
+import { getFromLocalStorage } from '@/lib/helper';
+
+import Accent from '@/components/Accent';
 import Button from '@/components/buttons/Button';
 import Layout from '@/components/layout/Layout';
 import Seo from '@/components/Seo';
 
 export default function UmamiPage() {
+  const [, setRerender] = React.useState(false);
+  function forceRerender() {
+    setRerender((t) => !t);
+  }
+
   function addUmami() {
+    forceRerender();
     if (typeof localStorage !== 'undefined') {
       return localStorage.setItem('umami.disabled', 'true');
     }
@@ -14,6 +23,7 @@ export default function UmamiPage() {
   }
 
   function removeUmami() {
+    forceRerender();
     if (typeof localStorage !== 'undefined') {
       return localStorage.removeItem('umami.disabled');
     }
@@ -21,6 +31,7 @@ export default function UmamiPage() {
   }
 
   function addIncrement() {
+    forceRerender();
     if (typeof localStorage !== 'undefined') {
       return localStorage.setItem('incrementMetaFlag', 'false');
     }
@@ -28,11 +39,16 @@ export default function UmamiPage() {
   }
 
   function removeIncrement() {
+    forceRerender();
     if (typeof localStorage !== 'undefined') {
       return localStorage.removeItem('incrementMetaFlag');
     }
     return null;
   }
+
+  const umamiDisabled = getFromLocalStorage('umami.disabled') === 'true';
+  const increaseViewDisabled =
+    getFromLocalStorage('incrementMetaFlag') === 'false';
 
   return (
     <Layout>
@@ -45,13 +61,27 @@ export default function UmamiPage() {
             <p className='mt-2 text-gray-600 dark:text-gray-300'>
               This is to block tracking.
             </p>
+            <div className='mt-4 space-y-2 text-sm'>
+              <p>
+                Umami:{' '}
+                <Accent className='font-semibold'>
+                  {umamiDisabled ? 'Not Tracking' : 'Tracking'}
+                </Accent>
+              </p>
+              <p>
+                View Count:{' '}
+                <Accent className='font-semibold'>
+                  {increaseViewDisabled ? 'Not Incrementing' : 'Incrementing'}
+                </Accent>
+              </p>
+            </div>
             <div className='flex flex-col gap-4 items-center mt-8'>
-              <Button onClick={addUmami}>add umami.disabled</Button>
-              <Button onClick={removeUmami}>remove umami.disabled</Button>
-              <Button onClick={addIncrement}>add incrementMetaFlag</Button>
+              <Button onClick={removeUmami}>allow track umami</Button>
+              <Button onClick={addUmami}>don't track umami</Button>
               <Button onClick={removeIncrement}>
-                remove incrementMetaFlag
+                allow increase view count
               </Button>
+              <Button onClick={addIncrement}>don't increase view count</Button>
             </div>
           </div>
         </section>
