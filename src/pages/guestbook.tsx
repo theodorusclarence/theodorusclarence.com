634cbc11e1f9333eee60b5e38c17d54773b01d6e
Theodorus Clarence <theodorusclarence@gmail.com>
2022-01-08 08:06:55 +0700

    feat: add AMA link on guestbook

--- src/pages/guestbook.tsx
+++ src/pages/guestbook.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import Accent from '@/components/Accent';
 import Comment from '@/components/content/Comment';
 import Layout from '@/components/layout/Layout';
+import CustomLink from '@/components/links/CustomLink';
 import Seo from '@/components/Seo';
 
 export default function GuestbookPage() {
@@ -21,6 +22,10 @@ export default function GuestbookPage() {
             </h1>
             <p className='mt-2 text-gray-700 dark:text-gray-200'>
               Leave whatever you like to say—message, appreciation, suggestions.
+              If you got some questions, you can leave them on the{' '}
+              <CustomLink href='https://github.com/theodorusclarence/theodorusclarence.com/discussions/179'>
+                AMA discussion
+              </CustomLink>
             </p>
             <figure className='mt-12'>
               <Comment />
