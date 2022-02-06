b9802b008e5f36df17318ee8750d9125050992a9
Theodorus Clarence <theodorusclarence@gmail.com>
2022-02-06 15:25:32 +0700

    test: group render pages

---
+++ cypress/integration/pages/render.spec.ts
@@ -0,0 +1,44 @@
+import { beforeEach, cy, describe, it } from 'local-cypress';
+
+describe('All Page', () => {
+  beforeEach(() => {
+    cy.window().then((win) =>
+      win.localStorage.setItem('umami.disabled', 'true')
+    );
+  });
+
+  it('should display index page', () => {
+    cy.visit('/');
+    cy.get('h1').should('contain', 'You can call me Clarence');
+  });
+
+  it('should display about page', () => {
+    cy.visit('/about');
+    cy.get('h1').should('contain', 'Theodorus Clarence');
+  });
+
+  it('should display blog page', () => {
+    cy.visit('/blog');
+    cy.get('h1').should('contain', 'Blog');
+  });
+
+  it('should display library page', () => {
+    cy.visit('/library');
+    cy.get('h1').should('contain', 'Library');
+  });
+
+  it('should display projects page', () => {
+    cy.visit('/projects');
+    cy.get('h1').should('contain', 'Projects');
+  });
+
+  it('should display subscribe page', () => {
+    cy.visit('/subscribe');
+    cy.get('h1').should('contain', 'Subscribe to theodorusclarence.com');
+  });
+
+  it('should display trf page', () => {
+    cy.visit('/trf');
+    cy.get('h1').should('contain', 'Rekening BCA');
+  });
+});
