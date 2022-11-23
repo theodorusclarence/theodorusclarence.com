e03f6fcdbe0d6b7efc2183d4675a50a6e5530300
Theodorus Clarence <theodorusclarence@gmail.com>
2022-11-23 11:18:30 +0700

    feat: add zod validation for api param

--- src/pages/api/newsletter/add.ts
+++ src/pages/api/newsletter/add.ts
@@ -1,18 +1,19 @@
 // eslint-disable-next-line unused-imports/no-unused-imports
 import axios, { AxiosError } from 'axios';
 import { NextApiRequest, NextApiResponse } from 'next';
+import { z } from 'zod';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method === 'POST') {
-    const data = req.body;
+    const email = z.string().email().parse(req.body.email);
 
     try {
       const response = await axios.post(
         'https://www.getrevue.co/api/v2/subscribers',
-        { ...data, double_opt_in: true },
+        { email, double_opt_in: true },
         {
           headers: {
             'Content-Type': 'application/json',
