f56ae81af92f8c4fb964799504f772a8a85b17bd
Theodorus Clarence <theodorusclarence@gmail.com>
2022-12-29 21:44:46 +0700

    feat: update trf bca and add mandiri

--- src/pages/trf/bca.tsx
+++ src/pages/trf/bca.tsx
@@ -3,7 +3,6 @@ import CopyToClipboard from 'react-copy-to-clipboard';
 
 import Accent from '@/components/Accent';
 import Button from '@/components/buttons/Button';
-import CloudinaryImg from '@/components/images/CloudinaryImg';
 import Layout from '@/components/layout/Layout';
 import Seo from '@/components/Seo';
 
@@ -18,8 +17,8 @@ export default function TrfPage() {
   return (
     <Layout>
       <Seo
-        templateTitle='Rekening Clarence'
-        description='Buka untuk melihat QR Code dan Nomor Rekening Clarence'
+        templateTitle='Rekening BCA Clarence'
+        description='Buka untuk melihat Nomor Rekening Clarence'
         robots='noindex,nofollow'
       />
 
@@ -27,21 +26,14 @@ export default function TrfPage() {
         <section className=''>
           <div className='layout flex flex-col items-center py-12 text-center'>
             <h1>Rekening BCA</h1>
-            <p className='mt-4 text-sm font-bold uppercase text-gray-500 dark:text-gray-400'>
+            <p className='mt-8 text-xs font-medium uppercase tracking-wider text-gray-500 dark:text-gray-400'>
               Atas Nama
             </p>
-            <h3 className='mt-1'>
+            <p className='mt-1 text-lg font-semibold tracking-wide'>
               <Accent>CLARENCE</Accent>
-            </h3>
-            <CloudinaryImg
-              className='mt-4 w-full max-w-sm'
-              publicId='theodorusclarence/qr-bca_rcbkew.jpg'
-              width={992}
-              height={886}
-              alt='QR BCA'
-            />
-            <div className='mt-8 flex flex-col items-center space-y-4'>
-              <code className='px-4 py-2 font-bold'>
+            </p>
+            <div className='mt-2 flex flex-col items-center space-y-4'>
+              <code className='px-4 py-2 text-lg font-semibold'>
                 <Accent>7630055037</Accent>
               </code>
               <CopyToClipboard
