b4ac62d8e59f4d56d62fa20a0b9ae9c4578cd31b
Vivienne Chen <vc@maltego.com>
2023-07-29 15:45:05 +0200

    feat: add gtm script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { AppProps } from 'next/app';
 import Router from 'next/router';
+import Script from 'next/script';
 import { ThemeProvider } from 'next-themes';
 import nProgress from 'nprogress';
 import * as React from 'react';
@@ -16,6 +17,34 @@ import '@/styles/nprogress.css';
 import { getFromLocalStorage } from '@/lib/helper';
 
 import { blockDomainMeta } from '@/constants/env';
+
+const GTM_ID = 'GTM-KD5JMLMM';
+
+const GtmScript = () => (
+  <>
+    <noscript>
+      <iframe
+        src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+        height='0'
+        width='0'
+        style={{ display: 'none', visibility: 'hidden' }}
+      />
+    </noscript>
+    <Script
+      id='gtm-script'
+      strategy='afterInteractive'
+      dangerouslySetInnerHTML={{
+        __html: `
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer', '${GTM_ID}');
+  `,
+      }}
+    />
+  </>
+);
 
 Router.events.on('routeChangeStart', nProgress.start);
 Router.events.on('routeChangeError', nProgress.done);
@@ -35,6 +64,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <ThemeProvider attribute='class' defaultTheme='dark' enableSystem={false}>
+      <GtmScript />
       <SWRConfig
         value={{
           fetcher: (url) => axios.get(url).then((res) => res.data),
