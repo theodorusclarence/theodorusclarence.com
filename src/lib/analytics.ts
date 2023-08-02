16f047680d175121a07e35b38ed24d2c569017f7
Theodorus Clarence <theodorusclarence@gmail.com>
2023-08-02 14:20:56 +0700

    feat: update umami event tracker

--- src/lib/analytics.ts
+++ src/lib/analytics.ts
@@ -1,20 +1,13 @@
-enum EventType {
-  'link',
-  'navigate',
-  'recommend',
-}
+const EVENT_TYPE = ['link', 'navigate', 'recommend'] as const;
+type EventType = (typeof EVENT_TYPE)[number];
 
 type TrackEvent = (
-  event_value: string,
-  event_type?: { [key: string]: string | number } & {
-    type: keyof typeof EventType;
-  },
-  url?: string | undefined,
-  website_id?: string | undefined
+  event_name: string,
+  event_data?: { type?: EventType } & { [key: string]: string | number }
 ) => void;
 
 export const trackEvent: TrackEvent = (...args) => {
-  if (window.umami && typeof window.umami.trackEvent === 'function') {
-    window.umami.trackEvent(...args);
+  if (window.umami && typeof window.umami.track === 'function') {
+    window.umami.track(...args);
   }
 };
