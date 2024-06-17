ab0d2c8a78670ea53e13cb66b6a9bf863119e3ed
emackinnon1 <emackinnon100390@gmail.com>
2024-06-16 20:53:21 -0600

    feat: add carousel

---
+++ src/components/images/Carousel.tsx
@@ -0,0 +1,59 @@
+import useEmblaCarousel from 'embla-carousel-react';
+import React from 'react';
+
+import {
+  NextButton,
+  PrevButton,
+  usePrevNextButtons,
+} from './CarouselArrowButtons';
+import { DotButton, useDotButton } from './CarouselDotButton';
+
+const Carousel = (props: any) => {
+  const { slides, options } = props;
+  const [emblaRef, emblaApi] = useEmblaCarousel(options);
+
+  const { selectedIndex, scrollSnaps, onDotButtonClick } =
+    useDotButton(emblaApi);
+
+  const {
+    prevBtnDisabled,
+    nextBtnDisabled,
+    onPrevButtonClick,
+    onNextButtonClick,
+  } = usePrevNextButtons(emblaApi);
+
+  return (
+    <section className='embla'>
+      <div className='embla__viewport' ref={emblaRef}>
+        <div className='embla__container'>
+          {slides.map((index: any) => (
+            <div className='embla__slide' key={index}>
+              <div className='embla__slide__number'>{index + 1}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className='embla__controls'>
+        <div className='embla__buttons'>
+          <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
+          <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+        </div>
+
+        <div className='embla__dots'>
+          {scrollSnaps.map((_: any, index: any) => (
+            <DotButton
+              key={index}
+              onClick={() => onDotButtonClick(index)}
+              className={'embla__dot'.concat(
+                index === selectedIndex ? ' embla__dot--selected' : ''
+              )}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Carousel;
---
+++ src/components/images/CarouselDotButton.tsx
@@ -0,0 +1,46 @@
+import React, { useCallback, useEffect, useState } from 'react';
+
+export const useDotButton = (emblaApi: any) => {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [scrollSnaps, setScrollSnaps] = useState([]);
+
+  const onDotButtonClick = useCallback(
+    (index: any) => {
+      if (!emblaApi) return;
+      emblaApi.scrollTo(index);
+    },
+    [emblaApi]
+  );
+
+  const onInit = useCallback((emblaApi: any) => {
+    setScrollSnaps(emblaApi.scrollSnapList());
+  }, []);
+
+  const onSelect = useCallback((emblaApi: any) => {
+    setSelectedIndex(emblaApi.selectedScrollSnap());
+  }, []);
+
+  useEffect(() => {
+    if (!emblaApi) return;
+
+    onInit(emblaApi);
+    onSelect(emblaApi);
+    emblaApi.on('reInit', onInit).on('reInit', onSelect).on('select', onSelect);
+  }, [emblaApi, onInit, onSelect]);
+
+  return {
+    selectedIndex,
+    scrollSnaps,
+    onDotButtonClick,
+  };
+};
+
+export const DotButton = (props: any) => {
+  const { children, ...restProps } = props;
+
+  return (
+    <button type='button' {...restProps}>
+      {children}
+    </button>
+  );
+};
